943599f1c7c9c488182156b7553285df9cf15109
Ez1n0 <ezino@outlook.com>
2024-08-17 12:19:12 +0000

    arquivos enviados para desenvolvimento Exercicio M19

--- gasolina.py
+++ gasolina.py
@@ -3,4 +3,4 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 df=pd.read_csv('gasolina.csv',delimiter=',')
 plt.figure(figsize=(8,6))
-sns.lineplot(data=df,x='dia',y='venda',color='red')
+sns.lineplot(data=df,x='dia',y='venda',color='red').set(title='Venda de gasolina em São Paulo - por dia - Jun/21',xlabel='Dia',ylabel='Venda')
